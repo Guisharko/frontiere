8047a8dc8cbca63198a57adbec0d4bade985fa31
guillaume.gallier <guillaume.gallier@gfi.fr>
2019-11-13 15:21:44 +0100

    maj

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,24 +4,32 @@ import {faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  constructor() {
-  }
-  faChevronDown = faChevronDown;
-  faChevronUp = faChevronUp;
-  public isCollapsed1 = true;
-  public isCollapsed2 = true;
-  public isCollapsed3 = true;
-  public isCollapsed4 = true;
+    constructor() {
+    }
 
-  ngOnInit() {
-    AOS.init({
-      duration: 2000,
-      delay: 1200,
-    });
-  }
+    faChevronDown = faChevronDown;
+    faChevronUp = faChevronUp;
+    public isCollapsed1 = false;
+    public isCollapsed2 = true;
+    public isCollapsed3 = true;
+    public isCollapsed4 = true;
+
+    public unCollapsed() {
+        this.isCollapsed1 = true;
+        this.isCollapsed2 = true;
+        this.isCollapsed3 = true;
+        this.isCollapsed4 = true;
+    }
+
+    ngOnInit() {
+        AOS.init({
+            duration: 2000,
+            delay: 1200,
+        });
+    }
 }
